c6cdcd44232c3c08e4ae445b659b19ed55b42c27
Fei <43999088+mirality@users.noreply.github.com>
2018-10-10 00:21:51 +0800

    Update start_app_server.sh

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://muser1:qwe123qwe@ds029827.mlab.com:29827/cooltest
 
 # finally, run the app in dev mode...
 npm run dev
